4291496f256f3279b8242269655b8c631c23bd12
LARS BERG <larsgbr@gmail.com>
2017-12-01 13:21:57 -0600

    Finished building website scraper

--- script1.py
+++ script1.py
@@ -10,34 +10,60 @@ all=soup.find_all("div",{"class":"propertyRow"})
 
 all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
 
+page_nr=soup.find_all("a",{"class":"Page"})[-1].text
+print(page_nr)
 #print(all)
+l=[]
 
-for item in all:
-	print(item.find("h4",{"class","propPrice"}).text.replace("\n","").replace("",""))
-	print(item.find_all("span",{"class","propAddressCollapse"})[0].text)
-	print(item.find_all("span",{"class","propAddressCollapse"})[1].text)
-	try:
-		print(item.find("span",{"class","infoBed"}).find("b").text)
-	except:
-		print(None)
-	
-	try:
-		print(item.find("span",{"class","infoSqFt"}).find("b").text)
-	except:
-		print(None)
-	
-	try:
-		print(item.find("span",{"class","infoValueFullBath"}).find("b").text)
-	except:
-		print(None)
-	
-	try:
-		print(item.find("span",{"class","infoValueHalfBath"}).find("b").text)
-	except:
-		print(None)
-	for column_group in item.find_all("div",{"class":"columnGroup"}):
-		#print(column_group)
-		for feature_group, feature_name in zip(column_group.find_all("span",{"class":"featureGroup"}),column_group.find_all("span",{"class":"featureName"})):
-			if "Lot Size" in feature_group.text:
-				print(feature_name.text)
-	print(" ")
+base_url="http://pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/#t=0&s="
+for page in range(0,int(page_nr)*10,10):
+	print(base_url+str(page)+".html.Paginate")
+	r=requests.get(base_url+str(page)+".html.Paginate")
+	c=r.content
+	soup=BeautifulSoup(c,"html.parser")
+	all=soup.find_all("div",{"class":"propertyRow"})
+
+	for item in all:
+		d={}
+		d["Address"] = item.find_all("span",{"class","propAddressCollapse"})[0].text
+		try:
+			d["Locality"] = item.find_all("span",{"class","propAddressCollapse"})[1].text
+		except:
+			d["Locality"] = None
+		d["Price"] = item.find("h4",{"class","propPrice"}).text.replace("\n","").replace("","")
+		try:
+			d["Beds"]=item.find("span",{"class","infoBed"}).find("b").text
+		except:
+			d["Beds"] = None
+		
+		try:
+			d["Area"]=item.find("span",{"class","infoSqFt"}).find("b").text
+		except:
+			d["Area"] = None
+		
+		try:
+			d["Full Baths"]=item.find("span",{"class","infoValueFullBath"}).find("b").text
+		except:
+			d["Full Baths"] = None
+		
+		try:
+			d["Half Baths"]=item.find("span",{"class","infoValueHalfBath"}).find("b").text
+		except:
+			d["Half Baths"] = None
+		for column_group in item.find_all("div",{"class":"columnGroup"}):
+			for feature_group, feature_name in zip(column_group.find_all("span",{"class":"featureGroup"}),column_group.find_all("span",{"class":"featureName"})):
+				if "Lot Size" in feature_group.text:
+					d["Lot Size"] = feature_name.text
+		l.append(d)
+		#print(l)
+
+import pandas
+df=pandas.DataFrame(l)
+df.to_csv("Output.csv")
+
+
+
+
+
+
+
